f9735fbb06e51cd824496186328bdf8788052cd3
Mohammad Baragbah <duniandewon@gmail.com>
2023-06-26 14:10:34 +0700

    remove redux path alias

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,6 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
       "@pages": path.resolve(__dirname, "./src/pages"),
       "@components": path.resolve(__dirname, "./src/components"),
-      "@redux": path.resolve(__dirname, "./src/redux"),
     },
   },
 });
