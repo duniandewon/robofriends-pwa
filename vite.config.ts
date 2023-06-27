43d874dc097b826bfc47391461a276064ef9a1a1
Mohammad Baragbah <duniandewon@gmail.com>
2023-06-27 16:07:03 +0700

    add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,43 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
 
 import path from "path";
 
+const manifestForPlugin: Partial<VitePWAOptions> = {
+  registerType: "prompt",
+  includeAssets: [],
+  manifest: {
+    short_name: "robofriends",
+    name: "Robofriends",
+    icons: [
+      {
+        src: "favicon.ico",
+        sizes: "64x64 32x32 24x24 16x16",
+        type: "image/x-icon",
+      },
+      {
+        src: "logo192.png",
+        type: "image/png",
+        sizes: "192x192",
+      },
+      {
+        src: "logo512.png",
+        type: "image/png",
+        sizes: "512x512",
+      },
+    ],
+    orientation: "portrait",
+    start_url: "/",
+    display: "standalone",
+    theme_color: "#000000",
+    background_color: "#ffffff",
+  },
+};
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(manifestForPlugin)],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
